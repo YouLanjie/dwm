02156e1cea5c9048b4fbeb58eab1feb38c15cb3a
YouLanjie <ylj18926385312@163.com>
2024-07-06 21:06:27 +0800

    更新脚本

--- script/statusbar.sh
+++ script/statusbar.sh
@@ -13,5 +13,5 @@
 while (( 1 )) {
 	xsetroot -name "^c#4B005B^^b#7799AA0x99^ Vol:`pactl get-sink-volume @DEFAULT_SINK@ |awk '{print $5}'|sed -n "1p"` ^c#4B005B^^b#7E51680x99^`date '+%Y年%m月%d日 %a %H:%M:%S'` "
 	sleep 0.1
-}
+}&
 
